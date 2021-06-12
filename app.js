e7f220667216ff8b18b484c75478eaf0eb2e20eb
meirianni <68259191+meirianni@users.noreply.github.com>
2021-06-12 19:14:06 +0700

    deploy heroku

--- app.js
+++ app.js
@@ -9,12 +9,15 @@ const session = require("express-session");
 
 // import moongose
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://127.0.0.1:27017/db_staycation", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-});
+mongoose.connect(
+  "mongodb+srv://meirianni:bwamern@cluster0.2zll9.mongodb.net/db_staycation?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  }
+);
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
